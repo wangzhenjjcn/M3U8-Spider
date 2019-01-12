8f03f37fc3f91f5c80553817a19249668a072f5d
WangZhen <wangzhenjjcn@gmail.com>
2019-01-12 09:10:58 +0800

    Update Spider.py

--- Spider.py
+++ Spider.py
@@ -1,8 +1,10 @@
-#coding=utf-8
+﻿#coding=utf-8
 import sys,time,datetime,os,urllib.request
-
+import io
+import sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 hdr = {
-       'Host':"wapp8.com",
+       'Host':"w------ap------p8.co------m",
        'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
        'Accept-Charset': 'utf-8;q=0.7,*;q=0.3',
@@ -11,12 +13,12 @@ hdr = {
        'Connection': 'keep-alive'
        }
 
-
-    
+domain="http://w------ap------p8.co------m/"
+flpg="?m=vod-type-id-16-pg-1.html"
  
 
 try:
-        ejurl=
+        ejurl=domain+flpg
         pagelb=urllib.request.urlopen(ejurl)
         print (ejurl)
 except Exception as e:
@@ -25,14 +27,14 @@ except Exception as e:
                                         
 else:
         if pagelb:
-                LBPageDetial=str(pagelb.read()).encode('utf8').decode("gb2312")
+                LBPageDetial=str(pagelb.read(),'utf8') 
                 LBPages=0
-                print(LBPageDetial)            
-                if ("下一页".encode('utf8')) in LBPageDetial and ("尾页".encode('utf8')) in LBPageDetial:
-                        print("00000000000")
-                        LBPages=LBPageDetial[LBPageDetial.index("下一页"):LBPageDetial.index("尾页")]
-                
-                
-                print (LBPages)
-        print ('6+56+5+6ioyhuyhuiyhui')				
+                LBLink=""
+                print(str(LBPageDetial))            
+                if ("下一页") in LBPageDetial and ("尾页") in LBPageDetial:
+                        LBPages=LBPageDetial[LBPageDetial.index("下一页"):LBPageDetial.index("尾页")]0
+                        LBLink=LBPages[LBPages.index("href=")+6:LBPages.index("pg-")+3]                        
+                        LBPages=LBPages[LBPages.index("pg-")+3:LBPages.index(".html")]
+                        print (LBPages)
+                        print ((domain+LBLink+LBPages+".html").replace(".com//",".com/"))
 				 
